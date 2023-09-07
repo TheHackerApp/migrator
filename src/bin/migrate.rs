0718d4bebc910d68af3e485fde5e363a8e75563e
Alexander Krantz <alex@krantz.dev>
2023-09-06 20:10:30 -0700

    Use shared logging crate (#6)
    Uses the internal [`logging`][logging] crate instead of manually
    implementing it. Also bumps the patch version to `0.1.3`.
    
    [logging]: https://github.com/TheHackerApp/logging

--- src/bin/migrate.rs
+++ src/bin/migrate.rs
@@ -7,8 +7,6 @@ use sqlx::{
 };
 use std::{path::PathBuf, str::FromStr};
 use tracing::{info, instrument, log::LevelFilter, Level};
-use tracing_error::ErrorLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -16,7 +14,9 @@ async fn main() -> eyre::Result<()> {
     dotenv()?;
 
     let config = Config::parse();
-    initialize_logging(config.log_level);
+    logging::config()
+        .default_directive(config.log_level)
+        .init()?;
 
     let migrator = Migrator::new(config.source)
         .await
@@ -69,22 +69,4 @@ async fn connect_to_database(url: &str) -> eyre::Result<PgPool> {
 
     info!("database connected");
     Ok(db)
-}
-
-/// Setup logging and error reporting
-fn initialize_logging(default_level: Level) {
-    Registry::default()
-        .with(
-            EnvFilter::builder()
-                .with_default_directive(default_level.into())
-                .from_env_lossy(),
-        )
-        .with(
-            tracing_subscriber::fmt::layer()
-                .with_file(true)
-                .with_line_number(true)
-                .with_target(true),
-        )
-        .with(ErrorLayer::default())
-        .init();
 }
